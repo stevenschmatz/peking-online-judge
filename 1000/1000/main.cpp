6facd02c03869bd7f1c6a5b7367542ca59596079
Steven Schmatz <stevenschmatz@gmail.com>
2014-03-16 15:05:55 -0400

    1000

---
+++ 1000/1000/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  1000
+//
+//  Created by Schmatz on 3/16/14.
+//  Copyright (c) 2014 Schmatz. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    int a,b;
+    std::cin >> a >> b;
+    std::cout << a + b << std::endl;
+    return 0;
+    
+}
